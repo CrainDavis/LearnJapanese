7ccce15a8d170c07a66f2ecf3610c02f69155913
Chyna Davis <chyna.davis11@gmail.com>
2020-09-21 09:24:57 -0700

    save highscores in local storage; navigate to highscores page

--- js/submit-score.js
+++ js/submit-score.js
@@ -6,6 +6,9 @@ const finalScore = document.getElementById("finalScore");
 
 // =======================================================================================
 
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 10;
+
 username.addEventListener("keyup", () => {
   saveScoreBtn.disabled = !username.value;
 });
@@ -16,5 +19,25 @@ finalScore.innerText = mostRecentScore;
 
 saveHighScore = (event) => {
   event.preventDefault();
-  console.log("save score button clicked!");
+
+  // items saved in local storage
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+
+  // push items into array
+  highScores.push(score);
+
+  // sort scores in order from highest to lowest
+  highScores.sort((a, b) => b.score - a.score);
+
+  // only save ten best scores
+  highScores.splice(10);
+
+  // save new items in local storage
+  localStorage.setItem("highScores", JSON.stringify(highScores));
+
+  // navigate to high scores page
+  window.location.assign("view-highscores.html");
 };
